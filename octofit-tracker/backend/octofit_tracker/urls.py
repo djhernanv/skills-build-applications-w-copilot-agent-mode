dcd4e1a1935212230f1050440e8024d575e0436f
Hernan Villamizar <hernan.villamizar@gmail.com>
2026-02-19 10:34:01 +0000

    tested server

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -52,22 +52,4 @@ urlpatterns = [
     path('', api_root, name='api-root'),
 ]
 
-import os
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    codespace_name = os.environ.get('CODESPACE_NAME')
-    if codespace_name:
-        base_url = f"https://{codespace_name}-8000.app.github.dev"
-    else:
-        base_url = "http://localhost:8000"
-    return Response({
-        'users': f"{base_url}{reverse('user-list', request=request, format=format)}",
-        'teams': f"{base_url}{reverse('team-list', request=request, format=format)}",
-        'activities': f"{base_url}{reverse('activity-list', request=request, format=format)}",
-        'leaderboard': f"{base_url}{reverse('leaderboard-list', request=request, format=format)}",
-        'workouts': f"{base_url}{reverse('workout-list', request=request, format=format)}",
-    })
